e2db3214ef166d0364caa88d33a3a60476f3b5d4
Francisco <frrgcsocial@gmail.com>
2018-06-23 12:47:43 +0200

    rere

--- prueba2/src/prueba2/fff.java
+++ prueba2/src/prueba2/fff.java
@@ -3,6 +3,6 @@ package prueba2;
 public class fff {
 	private void mai() {
 		// TODO Auto-generated method stub
-	 System.out.println("****FFFdddeeF");
+	 System.out.println("****FFFdddssseeF");
 	}
 }
